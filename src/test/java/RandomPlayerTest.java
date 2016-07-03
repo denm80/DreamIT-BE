5352a1c9b990455c1ac80ac5dc3d4b17142489f7
Denis Mikhailov <mikhailov.denis@tut.by>
2016-07-03 17:05:42 +0300

    comments for tests fix bug in RandomPlayerTest

--- src/test/java/RandomPlayerTest.java
+++ src/test/java/RandomPlayerTest.java
@@ -3,39 +3,48 @@ import org.junit.Test;
 
 import java.util.*;
 
+/**
+ * It is not very useful and reliable test, because we a going to check java.util.Random.
+ * But we can check whether behaviour of Player matches the hypothesis of a uniform distribution, at least.
+ * According table X^2 of Pearson criteria we have next list X_ CRITERIA_FOR ALPHA_900 of limit
+ * for confirm of uniform distribution with 75% probability.
+ * X_CRITERIA depends on freedom degrees, as well.  In our case it is third number.
+ * In our case it is 0.58437.
+ */
 public class RandomPlayerTest extends PlayerTest {
-    private static final int COUNT_ITERATION = 100;
-    private static final double[] X_CRITERIA_FOR_ALPHA_005 = { //ALPHA = 0.005
-            7.87944,
-            10.59663,
-            12.83816,
-            14.86026,
-            16.74960,
-            18.54758,
-            20.27774,
-            21.95495,
-            23.58935,
-            25.18818,
-            26.75685,
-            28.29952,
-            29.81947,
-            31.31935,
-            32.80132,
-            34.26719,
-            35.71847,
-            37.15645,
-            38.58226,
-            39.99685,
-            41.40106,
-            42.79565,
-            44.18128,
-            45.55851,
-            46.92789,
-            48.28988,
-            49.64492,
-            50.99338,
-            52.33562,
-            53.67196
+    private static final int COUNT_ITERATION = 1000;
+    private static final double[] X_CRITERIA_FOR_ALPHA_750 = { //ALPHA = 0.75
+            0.10153,
+            0.57536,
+            1.21253,
+            1.92256,
+            2.67460,
+            3.45460,
+            4.25485,
+            5.07064,
+            5.89883,
+            6.73720,
+            7.58414,
+            8.43842,
+            9.29907,
+            10.16531,
+            11.03654,
+            11.91222,
+            12.79193,
+            13.67529,
+            14.56200,
+            15.45177,
+            16.34438,
+            17.23962,
+            18.13730,
+            19.03725,
+            19.93934,
+            20.84343,
+            21.74940,
+            22.65716,
+            23.56659,
+            24.47761
+
     };
 
     @Test
@@ -53,7 +62,7 @@ public class RandomPlayerTest extends PlayerTest {
         double m = COUNT_ITERATION / degreesFreedom;
         double xObservable = results.values().stream().mapToDouble((i) -> Math.pow(i - m, 2) / m).sum();
 
-        Assert.assertTrue(xObservable < X_CRITERIA_FOR_ALPHA_005[degreesFreedom - 1]);
+        Assert.assertTrue(xObservable < X_CRITERIA_FOR_ALPHA_750[degreesFreedom - 1]);
     }
 
     @Override
--- src/test/java/RockPlayerTest.java
+++ src/test/java/RockPlayerTest.java
@@ -1,6 +1,10 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+/**
+ * It is not very useful and reliable test, because we cannot assure that this player will choose ROCK infinitely.
+ * We can check only first N results. For our task N is 100.
+*/
 public class RockPlayerTest extends PlayerTest {
     private static final int COUNT_ITERATION = 100;
 
